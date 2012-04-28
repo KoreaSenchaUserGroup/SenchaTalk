5228c8157b99d37a7e3ea290bcabd81e736e4f67
pikisvill <hanpool00@gmail.com>
2012-04-28 17:25:17 +0900

    chat

--- client/app/view/Chat.js
+++ client/app/view/Chat.js
@@ -8,7 +8,26 @@ Ext.define("SenchaTalk.view.Chat", {
 		scrollable: true,
 		styleHtmlContent: true,
 		html: [
-    		"I changed the default <b>HTML Contents</b> to something different!"
+			' <ul id="chat">                                               ' +
+			' <div id="nickname">                                          ' +
+			' 	<form id="set-nickname" class="wrap">                      ' +
+			' 		<p>Please type in your nickname and press enter.</p>   ' +
+			' 		<input id="nick">                                      ' +
+			' 		<p id="nickname-err">Nickname already in use</p>       ' +
+			' 	</form>                                                    ' +
+			' </div>                                                       ' +
+			' <div id="connecting">                                        ' +
+			' 	<div class="wrap">Connecting to socket.io server</div>     ' +
+			' </div>                                                       ' +
+			' <div id="messages">                                          ' +
+			' 	<div id="nicknames"></div>                                 ' +
+			' <div id="lines"></div>                                       ' +
+			' </div>                                                       ' +
+			' <form id="send-message">                                     ' +
+			' 	<input id="message">                                       ' +
+			' 	<button>Send</button>                                      ' +
+			' </form>                                                      ' +
+			' </ul>                                                        ' 
 		].join("")
 	}
 });
